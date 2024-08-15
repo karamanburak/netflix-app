eb1cb2f48501b0b6d8b95267de91642291719e0f
Burak Karaman <karaman.buraak@gmail.com>
2024-08-15 11:57:39 +0200

    Update Register page with form handling and authentication logic

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import GoogleIcon from "../../../../public/icons/GoogleIcon";
+"use client";
+import React, { useState } from "react";
+import GoogleIcon from "/public/icons/GoogleIcon";
+import useAuthCall from "@/hooks/useAuthCall";
 
 const Register = () => {
+  const [info, setInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const { createUser } = useAuthCall();
+
+  const { email, password, firstName, lastName } = info;
+
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName = `${firstName} ${lastName}`;
+    createUser(email, password, displayName);
+  };
+
   return (
     <main className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <div className="bg-black bg-opacity-70 p-16 self-center relative top-28 lg:w-2/5 lg:max-w-md w-full mx-auto">
-          <form>
+          <form onSubmit={handleSubmit}>
             <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3 ">
               Sign Up
             </h2>
@@ -17,6 +39,7 @@ const Register = () => {
                 required
                 placeholder=" "
                 name="firstName"
+                onChange={handleChange}
               />
               <label htmlFor="firstName">First Name</label>
             </div>
@@ -27,6 +50,7 @@ const Register = () => {
                 required
                 placeholder=" "
                 name="lastName"
+                onChange={handleChange}
               />
               <label htmlFor="lastName">Last Name</label>
             </div>
@@ -37,6 +61,7 @@ const Register = () => {
                 required
                 placeholder=" "
                 name="email"
+                onChange={handleChange}
               />
               <label htmlFor="email">Email</label>
             </div>
@@ -47,11 +72,15 @@ const Register = () => {
                 required
                 placeholder=" "
                 name="password"
+                onChange={handleChange}
               />
               <label htmlFor="password">Password</label>
             </div>
             <button className="btn-danger">Register</button>
-            <button className="btn-danger flex justify-between text-center ">
+            <button
+              type="button"
+              className="btn-danger flex justify-between text-center "
+            >
               Continue with Google
               <GoogleIcon />
             </button>
--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -1,14 +1,23 @@
 import { auth } from "@/auth/firebase";
+import { toastErrorNotify, toastSuccessNotify } from "@/helpers/ToastNotify";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const useAuthCall = () => {
-  const createUser = async (email, password) => {};
-  try {
-    createUserWithEmailAndPassword(auth, email, password);
-  } catch (error) {
-    console.log(error);
-  }
-
+  const router = useRouter();
+  const createUser = async (email, password, displayName) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      await updateProfile(auth.currentUser, {
+        displayName,
+      });
+      toastSuccessNotify("Register successfully");
+      router.push("/login");
+    } catch (error) {
+      toastErrorNotify(error.message);
+      console.log(error);
+    }
+  };
   return { createUser };
 };
 
