70f18cab9fb06dfd463b16063ae77719f5a64070
Burak Karaman <karaman.buraak@gmail.com>
2024-08-16 11:16:50 +0200

    Add remotePatterns for images in next.config.mjs

---
+++ src/app/(private)/movies/page.jsx
@@ -0,0 +1,27 @@
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
+
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  //   console.log(movies);
+
+  return (
+    <div>
+      <HeroSection
+        title={movies[0].title}
+        overview={movies[0].overview}
+        id={movies[0].id}
+      />
+      <div>
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMING" type="upcoming" />
+      </div>
+    </div>
+  );
+};
+
+export default Movies;
