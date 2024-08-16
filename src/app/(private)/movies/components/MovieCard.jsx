c8cfb7e81baed821a8d34f27063133bb4281a8a1
Burak Karaman <karaman.buraak@gmail.com>
2024-08-16 11:53:05 +0200

    Add getMovieDetail function to movieFunctions.js & fetch video details

--- src/app/(private)/movies/components/MovieCard.jsx
+++ src/app/(private)/movies/components/MovieCard.jsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
@@ -6,8 +8,12 @@ const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({ poster_path, vote_average, id }) => {
+  const router = useRouter();
   return (
-    <div className="w-40 h-[240] cursor-pointer relative">
+    <div
+      className="w-40 h-[240] cursor-pointer relative"
+      onClick={() => router.push("/movies/" + id)}
+    >
       <Image
         height={240}
         width={160}
